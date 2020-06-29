c6c1227eea82a005a5d37ac9396e45da44b3b9c1
Pravin Tripathi <pravinyo12@gmail.com>
2020-06-29 12:48:52 +0530

    user input added for start and end coordinate

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,8 @@
 #include "route_planner.h"
 
 using namespace std::experimental;
+using std::cout;
+using std::cin;
 
 static std::optional<std::vector<std::byte>> ReadFile(const std::string &path)
 {   
@@ -51,16 +53,29 @@ int main(int argc, const char **argv)
         else
             osm_data = std::move(*data);
     }
-    
-    // TODO 1: Declare floats `start_x`, `start_y`, `end_x`, and `end_y` and get
+
+    // Declare floats `start_x`, `start_y`, `end_x`, and `end_y` and get
     // user input for these values using std::cin. Pass the user input to the
     // RoutePlanner object below in place of 10, 10, 90, 90.
+    float start_x,start_y,end_x,end_y;
+    cout << "Enter Start X:";
+    cin >> start_x;
+
+    cout << "Enter Start Y:";
+    cin >> start_y;
+
+    cout << "Enter End X:";
+    cin >> end_x;
+
+    cout << "Enter End Y:";
+    cin >> end_y;
+
 
     // Build Model.
     RouteModel model{osm_data};
 
     // Create RoutePlanner object and perform A* search.
-    RoutePlanner route_planner{model, 10, 10, 90, 90};
+    RoutePlanner route_planner{model, start_x, start_y, end_x, end_y};
     route_planner.AStarSearch();
 
     std::cout << "Distance: " << route_planner.GetDistance() << " meters. \n";
