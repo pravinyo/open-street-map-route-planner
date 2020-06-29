77a037ef88fdfe81a8a12e4caef0a423eac21270
Pravin Tripathi <pravinyo12@gmail.com>
2020-06-29 15:47:07 +0530

    bug fixed for code 139

--- src/route_planner.cpp
+++ src/route_planner.cpp
@@ -1,9 +1,7 @@
 #include "route_planner.h"
 #include <algorithm>
 
-using namespace std;
-
-RoutePlanner::RoutePlanner(RouteModel &model, float start_x, float start_y, float end_x, float end_y): m_Model(model) {
+RoutePlanner::RoutePlanner(RouteModel &model, float start_x, float start_y, float end_x, float end_y) : m_Model(model) {
     // Convert inputs to percentage:
     start_x *= 0.01;
     start_y *= 0.01;
@@ -12,14 +10,9 @@ RoutePlanner::RoutePlanner(RouteModel &model, float start_x, float start_y, floa
 
     // TODO 2: Use the m_Model.FindClosestNode method to find the closest nodes to the starting and ending coordinates.
     // Store the nodes you find in the RoutePlanner's start_node and end_node attributes.
-    auto start = m_Model.FindClosestNode(start_x,start_y);
-    auto end = m_Model.FindClosestNode(end_x,end_y);
-
-    this->start_node = &start;
-    this->end_node = &end;
-
+    start_node = &m_Model.FindClosestNode(start_x, start_y);
+    end_node = &m_Model.FindClosestNode(end_x, end_y);
 }
-
 
 // TODO 3: Implement the CalculateHValue method.
 // Tips:
